27d74bd62721f28bf53b434a14ed58fa66082772
hzahradnik <horst.zahradnik@gmail.com>
2017-08-10 16:24:57 +0200

    added jsnlog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { JL } from 'jsnlog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(
+    @Inject('JSNLOG')
+    private JL: JL.JSNLog
+  ) {
+
+  }
+
+  ngOnInit() {
+    // Logging-Demo
+    //this.JL().trace("TRACE will only appear in Console");
+    //this.JL().debug("DEBUG will only appear in Console");
+    //this.JL().info("INFO will only appear in Console");
+    //this.JL().warn("WARN will only appear in Console");
+    //this.JL().error("ERROR will appear in DB&Console");
+    //this.JL().fatal("FATAL will appear in DB&Console");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import { JL } from 'jsnlog';
+
+export class UncaughtExceptionHandler implements ErrorHandler {
+  handleError(error: any) {
+    JL().fatalException('Uncaught Exception', error);
+  }
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +18,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [  
+    { provide: 'JSNLOG', useValue: JL },
+    { provide: ErrorHandler, useClass: UncaughtExceptionHandler }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
